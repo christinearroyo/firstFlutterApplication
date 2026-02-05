de16a2bc895df070e5f8660c7e99eb268bd36fba
Christine Arroyo <chrstinearroyo@gmail.com>
2026-02-05 08:45:57 +0800

    initial
    flutter & dart setup

---
+++ bin/conditionals.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+class Conditionals {
+
+}
+
+void conditionals() {
+  print("Sino kang gumagamit? ");
+
+  String? name = stdin.readLineSync();
+
+  print("Kamusta, $name \n Welcome sa Console App ko $name ^^");
+}
---
+++ bin/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  var name = "Christine";
+  int year = 2;
+  double gwa = 1.80;
+  var laptop = ["MSI", "Lenovo Legion", "ASUS ROG"];
+  var image = {
+    'tags' : ["Legion"],
+    'filepath' : 'C:\Users\CCS-205-021\Downloads\Tin\static\lenovo_legion.jpg',
+  };
+
+  print("Dart si Chris James Soriano!");
+  print("Name: $name, Year: $year");
+  print(gwa);
+  print(laptop);
+  print(image);
+}
+
